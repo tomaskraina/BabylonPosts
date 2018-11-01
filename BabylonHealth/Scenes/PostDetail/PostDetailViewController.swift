57ef261a39727c3becaaedaf0bd53d3ee5a2d2e9
Tom Kraina <tomas.kraina@gmail.com>
2018-11-01 08:28:32 +0000

    Add empty PostList and PostDetail scenes

---
+++ BabylonHealth/Scenes/PostDetail/PostDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PostDetailViewController.swift
+//  BabylonHealth
+//
+//  Created by Tom Kraina on 01/11/2018.
+//  Copyright © 2018 Tom Kraina. All rights reserved.
+//
+
+import UIKit
+
+class PostDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BabylonHealth/Scenes/PostList/PostListViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  PostListViewModel.swift
+//  BabylonHealth
+//
+//  Created by Tom Kraina on 01/11/2018.
+//  Copyright © 2018 Tom Kraina. All rights reserved.
+//
+
+import Foundation
