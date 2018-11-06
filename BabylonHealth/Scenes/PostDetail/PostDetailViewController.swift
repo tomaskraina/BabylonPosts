5306fdd2e1637cf37e6de66df75b636dfa3c9a89
Tom Kraina <tomas.kraina@gmail.com>
2018-11-06 09:56:40 +0000

    Implement PostDetail scene including PostDetailViewModel

--- BabylonHealth/Scenes/PostDetail/PostDetailViewController.swift
+++ BabylonHealth/Scenes/PostDetail/PostDetailViewController.swift
@@ -7,6 +7,38 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
+
+// TODO: Refactor to its own file
+extension UIFont {
+    static var caption: UIFont {
+        return UIFont.preferredFont(forTextStyle: .caption1)
+    }
+    
+    static var value: UIFont {
+        return UIFont.preferredFont(forTextStyle: .body)
+    }
+}
+
+
+// TODO: Refactor to its own file
+class CaptionValueLabelPair: UIView {
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var loadingValueContainer: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        captionLabel.font = .caption
+        valueLabel.font = .value
+        
+        loadingValueContainer.heightAnchor.constraint(greaterThanOrEqualToConstant: 28).isActive = true
+    }
+}
 
 class PostDetailViewController: UIViewController {
 
@@ -14,12 +46,70 @@ class PostDetailViewController: UIViewController {
     
     var viewModel: PostDetailViewModelling!
     
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var authorName: CaptionValueLabelPair!
+    @IBOutlet weak var postDescription: CaptionValueLabelPair!
+    @IBOutlet weak var numberOfComments: CaptionValueLabelPair!
+    
     // MARK: - UIViewController lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupBinding()
     }
 
+    // MARK: - Helpers
+    
+    private var disposeBag = DisposeBag()
+    
+    private func setupBinding() {
+        
+        viewModel.outputs.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: disposeBag)
+        
+        // MARK: author
+        
+        viewModel!.outputs.authorNameCaption
+            .drive(authorName.captionLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        viewModel.outputs.isLoadingAuthorName
+            .drive(authorName.activityIndicator.rx.isAnimating)
+            .disposed(by: disposeBag)
+        
+        viewModel!.outputs.authorName
+            .drive(authorName.valueLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        // MARK: comments
+        
+        viewModel!.outputs.numberOfCommentsCaption
+            .drive(numberOfComments.captionLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        viewModel.outputs.isLoadingNumberOfComments
+            .drive(numberOfComments.activityIndicator.rx.isAnimating)
+            .disposed(by: disposeBag)
+        
+        viewModel!.outputs.numberOfComment
+            .drive(numberOfComments.valueLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        // MARK: post description
+        
+        viewModel!.outputs.postDescriptionCaption
+            .drive(postDescription.captionLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        viewModel.outputs.isLoadingPostDescription
+            .drive(postDescription.activityIndicator.rx.isAnimating)
+            .disposed(by: disposeBag)
+        
+        viewModel!.outputs.postDescription
+            .drive(postDescription.valueLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
 }
--- BabylonHealth/Scenes/PostDetail/PostDetailViewModel.swift
+++ BabylonHealth/Scenes/PostDetail/PostDetailViewModel.swift
@@ -9,17 +9,27 @@
 import Foundation
 
 import RxSwift
+import RxCocoa
 import RxDataSources
 
 
 // MARK: - Protocols
 
-protocol PostDetailViewModelInputs {
-    
-}
+protocol PostDetailViewModelInputs {}
 
 protocol PostDetailViewModelOutputs {
+    var title: Driver<String> { get }
+    var isLoadingAuthorName: Driver<Bool> { get }
+    var authorName: Driver<String> { get }
+    var authorNameCaption: Driver<String> { get }
     
+    var isLoadingPostDescription: Driver<Bool> { get }
+    var postDescription: Driver<String> { get }
+    var postDescriptionCaption: Driver<String> { get }
+    
+    var isLoadingNumberOfComments: Driver<Bool> { get }
+    var numberOfComment: Driver<String> { get }
+    var numberOfCommentsCaption: Driver<String> { get }
 }
 
 protocol PostDetailViewModelling {
@@ -31,11 +41,104 @@ protocol PostDetailViewModelling {
 
 class PostDetailViewModel: PostDetailViewModelInputs, PostDetailViewModelOutputs {
     
+    var authorNameCaption: Driver<String> {
+        return Driver.just("Author") // TODO: L10n
+    }
+    
+    var postDescriptionCaption: Driver<String> {
+        return Driver.just("Body") // TODO: L10n
+    }
+    
+    var numberOfCommentsCaption: Driver<String> {
+        return Driver.just("Number of comments") // TODO: L10n
+    }
+    
+    
     typealias Dependencies = HasApiClient
     
     init(post: Post, dependencies: Dependencies) {
+        self.apiClient = dependencies.apiClient
+        self.post = Variable<Post>(post)
         
+        // TODO: Do this differently as apiClient is captured
+        self.user = self.post.asObservable().flatMap({ [apiClient] (post) -> Observable<User> in
+            return apiClient.requestUserList()
+                .debug("users request", trimOutput: true)
+                .map({ $0.first(where: { $0.id == post.userID }) })
+                .flatMap({ value -> Observable<User> in
+                    if let value = value {
+                        return Observable.just(value)
+                    } else {
+                        return Observable.empty()
+                    }
+                }).debug("user", trimOutput: true)
+        })
+        
+        self.comments = self.post.asObservable()
+            .flatMap({ [apiClient] (post) -> Observable<Comments> in
+            return apiClient.requestComments(postId: post.id)
+            }).debug("comments", trimOutput: true)
     }
+    
+    var title: Driver<String> {
+        return Driver.just("Post detail") // TODO: L10n
+    }
+    
+    var authorName: Driver<String> {
+        return user.map({ $0.username })
+            .do(onNext: { [weak self] (_) in
+                self?.isLoadingUserVariable.value = false
+                }, onSubscribe: { [weak self] in
+                    self?.isLoadingUserVariable.value = true
+            }).startWith("")
+            .asDriver(onErrorJustReturn: "N/A")
+    }
+    
+    var postDescription: Driver<String> {
+        return post.asObservable().map({ $0.body })
+            .startWith("")
+            .asDriver(onErrorJustReturn: "N/A")
+    }
+    
+    var numberOfComment: Driver<String> {
+        return comments
+            .map({ $0.count })
+            .map(String.init)
+            .do(onNext: { [weak self] (_) in
+                self?.isLoadingCommentsVariable.value = false
+                }, onSubscribe: { [weak self] in
+                    self?.isLoadingCommentsVariable.value = true
+            })
+            .startWith("")
+            .asDriver(onErrorJustReturn: "N/A")
+    }
+    
+    var isLoadingAuthorName: Driver<Bool> {
+        return isLoadingUserVariable.asDriver()
+    }
+    
+    var isLoadingPostDescription: Driver<Bool> {
+        return Driver.just(false)
+    }
+    
+    var isLoadingNumberOfComments: Driver<Bool> {
+        return isLoadingCommentsVariable.asDriver()
+    }
+    
+    
+    // MARK: - Privates
+    
+    private let post: Variable<Post>
+    
+    private let apiClient: ApiClient
+    
+    private let user: Observable<User>
+    
+    private let comments: Observable<Comments>
+    
+    private let isLoadingUserVariable: Variable<Bool> = .init(false)
+    
+    private let isLoadingCommentsVariable: Variable<Bool> = .init(false)
     
 }
 
