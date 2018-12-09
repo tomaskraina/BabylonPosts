2650917c5ff183a78f8c0b3238b2e2f046718eee
Tom Kraina <tomas.kraina@gmail.com>
2018-12-09 10:46:22 +0100

    Test against protocol and not concrete type RealmPersistanceStorage

--- BabylonHealthTests/RealmPersistentStorageTests.swift
+++ BabylonHealthTests/RealmPersistentStorageTests.swift
@@ -34,11 +34,11 @@ class RealmPersistentStorageTests: XCTestCase {
         let posts: Posts = try JSON(named: "posts")
         
         // When
-        let storage = RealmPersistantStorage()
+        let storage: PersistentStorage = RealmPersistantStorage()
         storage.storePosts().onNext(posts)
         
         // Then
-        let storage2 = RealmPersistantStorage()
+        let storage2: PersistentStorage = RealmPersistantStorage()
         let result = try storage2.posts()
             .toBlocking(timeout: 1)
             .first()
@@ -53,17 +53,16 @@ class RealmPersistentStorageTests: XCTestCase {
         let users: Users = try JSON(named: "users")
         
         // When
-        let storage = RealmPersistantStorage()
+        let storage: PersistentStorage = RealmPersistantStorage()
         storage.storeUsers().onNext(users)
         
         // Then
-        let storage2 = RealmPersistantStorage()
-        let result = try storage2.users()
+        let storage2: PersistentStorage = RealmPersistantStorage()
+        let result = try storage2.user(id: 1)
             .toBlocking(timeout: 1)
             .first()
         
         XCTAssertNotNil(result)
-        XCTAssertEqual(result?.count, 10)
     }
 
     
@@ -73,11 +72,11 @@ class RealmPersistentStorageTests: XCTestCase {
         let comments: Comments = try JSON(named: "comments")
         
         // When
-        let storage = RealmPersistantStorage()
+        let storage: PersistentStorage = RealmPersistantStorage()
         storage.storeComments().onNext(comments)
         
         // Then
-        let storage2 = RealmPersistantStorage()
+        let storage2: PersistentStorage = RealmPersistantStorage()
         let result = try storage2.commentCount(for: 1)
             .toBlocking(timeout: 1)
             .first()
